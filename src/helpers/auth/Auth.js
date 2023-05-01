2f951bfa8d923189be56ab0264bd6d83f21bc3d5
efnetoworks <74969420+efnetoworks@users.noreply.github.com>
2023-05-01 09:28:51 +0100

    ikl

--- src/helpers/auth/Auth.js
+++ src/helpers/auth/Auth.js
@@ -1,1 +1,14 @@
+import Api from "../Api";
 
+export default {
+    login(form: object){
+        return Api.post('/login', form)
+    },
+    logout() {
+        return Api.post('/logout')
+    },
+    authUser(){
+        return Api.post('/auth-agent')
+    }
+    
+}
