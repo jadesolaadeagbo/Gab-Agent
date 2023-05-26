9ccb39cd63e2d787685852e80097cee08603e9cb
Ikenna Anikwe <31927515+iyke0z@users.noreply.github.com>
2023-05-26 08:08:29 +0100

    fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,9 +4,9 @@
             <div class="absolute">
                 <p class="w-[166px] h-[39px] top-[10px] left-[32px] absolute not-italic text-sm font-bold flex items-center text-grey">
                     {{ label }}
-                </p>
+                </p>                
                 <!-- <img src="/public/assets/info.png" alt="" class="w-[16px] h-[16px] top-[42px] left-[332px] absolute not-italic text-sm font-bold flex items-center text-grey"> -->
-                <img :src="'/public/assets/'+img" class="relative w-[80px] h-[68px] left-[32px] top-[70px] rounded-[18px]" alt="">
+                <img :src="'/assets/'+img" class="relative w-[80px] h-[68px] left-[32px] top-[70px] rounded-[18px]" alt="">
                 <p 
                     class="relative not-italic font-bold text-md flex flex-row items-center text-center top-[80px] left-[32px]">
                     {{amount}}
